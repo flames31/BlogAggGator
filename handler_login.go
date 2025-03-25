5de91387ecffb79aca34c10e639b933b0d6b7e23
flames31 <rrahulao2000@gmail.com>
2025-03-25 15:07:12 -0400

    Added reset command

--- handler_login.go
+++ handler_login.go
@@ -3,6 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"github.com/flames31/BlogAggGator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -23,5 +27,30 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	fmt.Printf("Switched to user : %v\n", usrName)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage : %s <username>", cmd.Name)
+	}
+
+	usrName := cmd.Args[0]
+
+	dbUser, err := s.db.CreateUser(context.Background(), database.CreateUserParams{ID: uuid.New(), CreatedAt: time.Now(), UpdatedAt: time.Now(), Name: usrName})
+	if err != nil {
+		return fmt.Errorf("error while creating user: %w", err)
+	}
+	s.cfg.SetUser(dbUser.Name)
+
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.DeleteAllUsers(context.Background())
+	if err != nil {
+		return err
+	}
+	fmt.Println("All rows removed from the users table")
 	return nil
 }
